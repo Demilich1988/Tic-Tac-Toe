ec231acfff1da8d688c4e2005c8da13e0b8b9181
Chris Langford <demilich1988@gmail.com>
2017-05-02 10:32:03 -0400

    Add project files.

---
+++ Tic Tac Toe/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
